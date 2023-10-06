72697c463f37ee0198a5696f73f127830e10c5f4
Lee Yu Chieh <winnie524722@gmail.com>
2023-10-06 15:25:57 +0800

    feat: 新增部署前端github page和後端Render的設定

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <IssueDataContextProvider>
       <ModalContextProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/github_project_management">
           <Routes>
             <Route path="home" element={<Home />} />
             <Route path="redirect" element={<Redirect />} />
--- src/contexts/IssueDataContext.tsx
+++ src/contexts/IssueDataContext.tsx
@@ -44,7 +44,7 @@ type ContextType = {
   logout: () => void;
 };
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://github-project-management.onrender.com";
 
 const IssueDataContext = createContext<ContextType | null>(null);
 
