8945b9f04ae09d361905e60dd1b4d43598c7cdad
Lee Yu Chieh <winnie524722@gmail.com>
2023-10-11 15:51:18 +0800

    fix: 將BrowserRouter改為HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Issue from "./pages/Issue";
 import Redirect from "./pages/Redirect";
@@ -9,14 +9,14 @@ function App() {
   return (
     <IssueDataContextProvider>
       <ModalContextProvider>
-        <BrowserRouter basename="/github_project_management">
+        <HashRouter>
           <Routes>
             <Route path="home" element={<Home />} />
             <Route path="redirect" element={<Redirect />} />
             <Route path="issue" element={<Issue />} />
             <Route path="*" element={<Navigate to="/home" replace />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </ModalContextProvider>
     </IssueDataContextProvider>
   );
